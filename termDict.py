c13b643d7e0036c8cf8c3b6952c412d36d9d2da1
jonnySheldon <ewart.jonny@gmail.com>
2021-01-04 14:08:08 +0000

    initial commit

---
+++ termDict.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jan  4 13:02:18 2021
+
+@author: jonnysheldon
+"""
+
+
+
+term_dict = {
+"names" : ["name","surname"],
+"addresses" : ["address", "city", "country", "PO Box"],
+"numbers" : ["NHS number","phone number", "Hospital number", "other"],
+"date" : ["day","month","year"],
+"tag_type" : ["clinician", "patient/other"]
+    }
+
+print(term_dict)
