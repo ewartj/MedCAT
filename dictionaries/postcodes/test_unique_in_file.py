45df659313f0c161667a5626f7816be8e3e2b236
jonnySheldon <ewart.jonny@gmail.com>
2021-01-21 09:03:31 +0000

    made unique list of postcodes

--- dictionaries/postcodes/test_unique_in_file.py
+++ dictionaries/postcodes/test_unique_in_file.py
@@ -1,4 +1,4 @@
-with open('test_all.txt') as file:
+with open('all_postcodes.txt') as file:
     lines = [line.rstrip('\n') for line in file]
 
 length_list = len(lines)
@@ -6,3 +6,10 @@ set_list = set(lines)
 set_list_length = len(set_list)
 
 print(f"List is {length_list} and has {set_list_length} unique values")
+
+unique_list = list(set_list)
+
+with open('final_postcode_list.txt', 'w') as f:
+    for i in range(0,len(unique_list)):
+        f.write("%s\n" % unique_list[i])
+
